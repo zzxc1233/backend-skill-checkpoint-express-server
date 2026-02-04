5d0324662a8dbc3b0f5d8f65e1dc8b7364c9e77c
champ <champdrm@gmail.com>
2026-02-04 16:07:17 +0700

    feature(docs): adds Swagger API documentation

--- app.mjs
+++ app.mjs
@@ -1,4 +1,8 @@
 import express from "express";
+
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
 import questionRouter from "./routes/questionRouter.mjs";
 import answerRouter from "./routes/answerRouter.mjs";
 
@@ -9,6 +13,72 @@ app.use(express.json());
 app.use("/questions", questionRouter);
 app.use("/answers", answerRouter);
 
+// swagger definition
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Question & Answer API",
+      version: "1.0.0",
+      description: "API for a Question and Answer platform",
+    },
+  },
+  apis: ["app.mjs"],
+};
+
+// swagger documentation
+const swaggerSpec = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * /questions:
+ *   get:
+ *     summary: Get all questions
+ *     responses:
+ *       200:
+ *         description: OK
+ */
+
+/**
+ * @swagger
+ * /questions:
+ *   post:
+ *     summary: Create a new question
+ *     responses:
+ *       201:
+ *         description: Question created successfully.
+ */
+
+/**
+ * @swagger
+ * /questions/search:
+ *   get:
+ *     summary: Search questions by title or category
+ *     responses:
+ *       200:
+ *         description: OK
+ */
+
+/**
+ * @swagger
+ * /questions/{questionId}:
+ *   get:
+ *     summary: Get a question by ID
+ *     parameters:
+ *       - name: questionId
+ *         in: path
+ *         required: true
+ *         description: ID of the question to retrieve
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: OK
+ */
+
+
+// test route
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
