449c5ab7dcc63d32a246e5c058f69e5443eb239d
champ <champdrm@gmail.com>
2026-02-04 15:02:20 +0700

    feature(package): sets up PostgreSQL client and ES module support

--- utils/db.mjs
+++ utils/db.mjs
@@ -4,7 +4,7 @@ const { Pool } = pg.default;
 
 const connectionPool = new Pool({
   connectionString:
-    "postgresql://your-db-username:your-db-password@localhost:5432/your-db-name",
+    "postgresql://postgres:newpassword@localhost:5432/checkpoint-backend",
 });
 
 export default connectionPool;
