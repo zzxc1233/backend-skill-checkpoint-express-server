269431f36dc26d6d2e07f24141db9b9e9145fb96
champ <champdrm@gmail.com>
2026-02-04 15:11:25 +0700

    feature(api): adds comprehensive answer management API

--- app.mjs
+++ app.mjs
@@ -1,11 +1,13 @@
 import express from "express";
 import questionRouter from "./routes/questionRouter.mjs";
+import answerRouter from "./routes/answerRouter.mjs";
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
 app.use("/questions", questionRouter);
+app.use("/answers", answerRouter);
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
